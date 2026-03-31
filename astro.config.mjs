77a53707efde3249b17a4451b453110712ba5ff7
bhav-bains <bhav@fadeawaycreatives.com>
2026-03-30 21:29:54 -0700

    feat: implement contact page, SEO meta, full-screen mobile menu, and layout refinements

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://fadeawaycreatives.com",
   integrations: [tailwind(), react(), mdx()],
   vite: {
     ssr: {
